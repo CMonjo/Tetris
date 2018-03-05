b5eef18308b7b0f9439a899c36e08b86a223bca5
Dakiche <raphael2.dakiche@epitech.eu>
2018-03-05 01:19:41 +0100

    (ADD) -h

--- lib/my/my_strcmp.c
+++ lib/my/my_strcmp.c
@@ -4,9 +4,10 @@
 ** File description:
 ** c
 */
-int		my_strcmp(char const *s1, char const *s2)
+
+int my_strcmp(char const *s1, char const *s2)
 {
-	int		i = 0;
+	int i = 0;
 
 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 		i++;
--- src/main.c
+++ src/main.c
@@ -18,18 +18,30 @@ void free_tetris(layers_t *layers, piece_t *pieces)
 	free(pieces);
 }
 
+int tetris_help(int ac, char **av)
+{
+	char *help;
+
+	if (ac == 2 && my_strcmp(av[1], "-help")) {
+		help = my_read("text/help.txt");
+		my_putstr(help);
+		free(help);
+		return (1);
+	}
+	return (0);
+}
+
 int main(int ac, char **av)
 {
 	layers_t *layers;
 	piece_t *pieces;
 
-	if (ac != 1)
-		return (84);
+	if (tetris_help(ac, av) == 1)
+		return (0);
 	layers = fill_layers();
 	pieces = create_pieces();
 	if (!layers || !pieces)
 		return (84);
-	av = av;
 	display_tetris(layers, pieces);
 	free_tetris(layers, pieces);
 	return (0);
